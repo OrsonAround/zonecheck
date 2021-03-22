1f707459adecf9774adef1aaec9c5fa9284871c8
OrsonAround <samuel.reamer@gmail.com>
2021-03-21 22:11:43 -0400

    boom

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 exclude="${dir}/rsync-exclude.txt"
-src="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../dist" && pwd )/"
-dest="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../hab/openhab_conf" && pwd )"
-rsync --archive --verbose --progress --stats --checksum --exclude-from=${exclude} $src $dest
+src="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../src" && pwd )/"
+dest="/etc/openhab"
+touch "${src}automation/jsr223/zonecheck/rules.js"
+rsync --archive --verbose --progress --stats --exclude-from=${exclude} $src $dest
+sleep 3
+curl -X PUT --header "Content-Type: text/plain" --header "Accept: application/json" -d "RUN" "http://127.0.0.1:8080/rest/items/Z_Trigger/state"
